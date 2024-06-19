4368533b1bf89aca8b00017487b79aa21ec87ba2
Benny Schneider <stupacki86@gmail.com>
2024-06-19 14:43:56 +0200

    Ad DI example

---
+++ 01-Pattern/pattern/src/main/java/io/stupacki/pattern/creational/DependencyInjection.kt
@@ -0,0 +1,77 @@
+package io.stupacki.pattern.creational
+
+/*
+ * Dependency Injection
+ *
+ * A class accepts the objects it requires from an injector instead of creating the objects directly.
+ *
+ *
+ */
+
+// Implementation
+
+// Component
+interface DiComponentInterface {
+    val modules: List<DiModule>
+    fun <T : Any> inject(target: Class<T>): T
+}
+
+//Module
+interface DiModuleInterface<T : Any> {
+    var targets: Map<Class<*>, T>
+    fun addTarget(value: Any)
+}
+
+class DiModule : DiModuleInterface<Any> {
+    override var targets: Map<Class<*>, Any> = emptyMap()
+
+    override fun addTarget(value: Any) {
+        targets += value::class.java to value
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class DiComponent(
+    override val modules: List<DiModule> = emptyList(),
+) : DiComponentInterface {
+
+    override fun <T : Any> inject(target: Class<T>): T {
+        modules.forEach { module ->
+            module.targets.forEach { moduleTarget ->
+                if (moduleTarget.key == target) {
+                    return moduleTarget.value as T
+                }
+            }
+        }
+        throw IllegalArgumentException("Target not found")
+    }
+}
+
+// Usage
+
+// Component
+fun mainComponent(): DiComponent = DiComponent(
+    modules = listOf(mainModule())
+)
+
+// Module
+fun mainModule(): DiModule = DiModule().apply {
+    addTarget(MainViewModel())
+}
+
+
+class MainViewModel {
+    val state = listOf("INITIAL_STATE")
+}
+
+object TargetView {
+
+    private val module: DiComponent = mainComponent()
+    private val mainViewModel: MainViewModel = module.inject(MainViewModel::class.java)
+
+    init {
+        mainViewModel.state.forEach { state ->
+            println(state)
+        }
+    }
+}
